6e43cd879192f440a419abc982dbd06bc4ff186f
Ando Rakotonandrasana <ando-sitraka-2.rakotonandrasana@polymtl.ca>
2025-04-09 15:15:42 -0400

    test

--- python/multimodalsim/simulator/simulation.py
+++ python/multimodalsim/simulator/simulation.py
@@ -18,7 +18,7 @@ from multimodalsim.simulator.vehicle_event import VehicleReady
 
 logger = logging.getLogger(__name__)
 
-
+# Classe principale représentant la simulation multimodale
 class Simulation:
 
     def __init__(self, optimization: Optimization, trips: list[Trip],
@@ -29,23 +29,31 @@ class Simulation:
                  coordinates: Optional[Coordinates] = None,
                  travel_times: Optional[TravelTimes] = None,
                  config: Optional[str | SimulationConfig] = None) -> None:
-
+        """
+        Initialisation de la simulation avec tous les composants nécessaires : optimisation, trajets, véhicules, etc.
+        """
+         # Initialisation de l'environnement avec la configuration donnée
         self.__env = Environment(optimization, network=network,
                                  coordinates=coordinates,
                                  travel_times=travel_times)
+        # Initialisation de la file d'attente des événements
         self.__queue = EventQueue(self.__env)
+         # Initialisation de l'observateur de l'environnement
         self.__environment_observer = environment_observer
-
+        # Chargement de la configuration de la simulation
         self.__load_config(config)
-
+        # Création des événements pour chaque véhicule (VehicleReady)
         self.__create_vehicle_ready_events(vehicles, routes_by_vehicle_id)
-
+        # Création des événements de libération des passagers (PassengerRelease)
         self.__create_passenger_release_events(trips)
-
+        # Initialisation de l'heure de départ
         self.__initialize_time(vehicles, trips)
 
     @property
     def data_collectors(self) -> Optional[list[DataCollector]]:
+        """
+        Récupère les collecteurs de données s'ils sont définis dans l'observateur de l'environnement.
+        """
         if self.__environment_observer is not None:
             data_collectors = self.__environment_observer.data_collectors
         else:
@@ -53,41 +61,55 @@ class Simulation:
         return data_collectors
 
     def simulate(self, max_time: Optional[float] = None) -> None:
+        """
+        Effectue la simulation en fonction de la file d'événements. La simulation s'arrête 
+        lorsque la file est vide ou que le temps maximal est atteint.
+        """
         max_time = self.__max_time if max_time is None else max_time
 
-        # main loop of the simulation
+        # Boucle principale de la simulation
         while not self.__queue.is_empty():
 
-            current_event = self.__queue.pop()
+            current_event = self.__queue.pop() # Récupère l'événement suivant dans la file d'attente
 
-            self.__env.current_time = current_event.time
+
+            self.__env.current_time = current_event.time # Mise à jour du temps actuel de l'environnement
 
             if max_time is not None and self.__env.current_time > max_time:
-                break
-
+                break # Arrêter la simulation si le temps maximal est dépassé
+            # Visualisation de l'environnement au moment de l'événement
             self.__visualize_environment(current_event, current_event.index,
                                          current_event.priority)
-
+            # Traitement de l'événement
             process_event = current_event.process(self.__env)
             logger.debug("process_event: {}".format(process_event))
+            # Collecte de données sur l'événement en cours
             self.__collect_data(current_event, current_event.index,
                                 current_event.priority)
-
+        # Fin de la simulation
         logger.info("\n***************\nEND OF SIMULATION\n***************")
         self.__visualize_environment()
 
     def __load_config(self, config):
+        """
+        Charge la configuration de la simulation, soit depuis un fichier de configuration,
+        soit en utilisant la configuration par défaut.
+        """
         if isinstance(config, str):
-            config = SimulationConfig(config)
+            config = SimulationConfig(config) # Chargement de la configuration à partir d'un fichier
         elif not isinstance(config, SimulationConfig):
-            config = SimulationConfig()
-
+            config = SimulationConfig() # Utilisation de la configuration par défaut
+        # Initialisation des paramètres de la simulation à partir de la configuration
         self.__max_time = config.max_time
         self.__speed = config.speed
         self.__time_step = config.time_step
         self.__update_position_time_step = config.update_position_time_step
 
     def __create_vehicle_ready_events(self, vehicles, routes_by_vehicle_id):
+        """
+        Crée les événements pour chaque véhicule (VehicleReady).
+        Ces événements sont ajoutés à la file d'attente pour être traités.
+        """
         for vehicle in vehicles:
             route = routes_by_vehicle_id[vehicle.id] \
                 if vehicle.id in routes_by_vehicle_id else None
@@ -96,16 +118,25 @@ class Simulation:
                          self.__update_position_time_step).add_to_queue()
 
     def __create_passenger_release_events(self, trips):
+        """
+        Crée les événements de libération des passagers (PassengerRelease).
+        Chaque événement est associé à un trajet et ajouté à la file d'attente.
+        """
         for trip in trips:
             PassengerRelease(trip, self.__queue).add_to_queue()
 
     def __initialize_time(self, vehicles, trips):
+        """
+        Initialise l'heure de départ de la simulation en fonction des véhicules et des trajets.
+        """
+        # Recherche du temps de libération le plus tôt parmi les véhicules
         first_vehicle_event_time = self.__find_smallest_release_time(vehicles)
+        # Recherche du temps de libération le plus tôt parmi les trajets
         first_event_time = self.__find_smallest_release_time(
             trips, first_vehicle_event_time)
 
-        self.__env.current_time = first_event_time
-
+        self.__env.current_time = first_event_time # Initialisation du temps de départ
+        # Si un pas de temps est défini, un événement de synchronisation récurrent est ajouté
         if self.__time_step is not None:
             RecurrentTimeSyncEvent(self.__queue, first_event_time,
                                    self.__time_step,
@@ -113,6 +144,9 @@ class Simulation:
 
     def __find_smallest_release_time(self, objects_list,
                                      smallest_release_time=None):
+        """
+        Trouve le temps de libération le plus tôt parmi une liste d'objets.
+        """
         if smallest_release_time is None:
             smallest_release_time = objects_list[0].release_time \
                 if len(objects_list) > 0 else None
@@ -125,14 +159,20 @@ class Simulation:
 
     def __visualize_environment(self, current_event=None, event_index=None,
                                 event_priority=None):
+        """
+        Visualise l'environnement à chaque événement, si un observateur est défini.
+        """
         if self.__environment_observer is not None:
             for visualizer in self.__environment_observer.visualizers:
                 visualizer.visualize_environment(self.__env, current_event,
                                                  event_index,
                                                  event_priority)
 
-    def __collect_data(self, current_event=None, event_index=None,
+    def __collect_data(self, current_event=None, event_index=None, 
                        event_priority=None):
+        """
+        Collecte des données sur l'événement en cours via les collecteurs de données définis dans l'observateur.
+        """
         if self.__environment_observer is not None:
             for data_collector in self.__environment_observer.data_collectors:
                 data_collector.collect(self.__env, current_event,
--- python/multimodalsim/simulator/stop.py
+++ python/multimodalsim/simulator/stop.py
@@ -4,39 +4,59 @@ import multimodalsim.simulator.request as request
 
 
 class Stop:
-    """A stop is located somewhere along the network.  New requests
-    arrive at the stop.
+    """
+    Un arrêt représente un lieu où un véhicule arrive pour prendre ou déposer des passagers. Il contient des informations
+    sur l'heure d'arrivée et de départ, les passagers concernés, et la localisation de l'arrêt dans le réseau.
+    
+    Attributs :
     ----------
-    arrival_time: float
-        Date and time at which the vehicle arrives the stop
-    departure_time: float
-        Date and time at which the vehicle leaves the stop
-    min_departure_time: float
-        Minimum time at which the vehicle is allowed to leave the stop
-    cumulative_distance: float
-        Cumulative distance travelled by the vehicle when arriving at the stop
-    passengers_to_board: list of Trip objects
-        list of passengers who need to board
-    boarding_passengers: list of Trip objects
-        list of passengers who are boarding
-    boarded_passengers: list of Trip objects
-        list of passengers who are already boarded
-    passengers_to_alight: list of Trip objects
-        list of passengers to alight
-        OLD: list of passengers who are alighted
-    alighted_passengers: list of Trip objects
-        list of passengers who are alighted
-    location: Location
-        Object of type Location referring to the location of the stop
-        (e.g., GPS coordinates)
+    arrival_time : float
+        Heure d'arrivée du véhicule à l'arrêt.
+    departure_time : float
+        Heure de départ du véhicule de l'arrêt.
+    min_departure_time : float
+        Heure minimale à laquelle le véhicule peut quitter l'arrêt.
+    cumulative_distance : float
+        Distance cumulée parcourue par le véhicule à son arrivée à l'arrêt.
+    passengers_to_board : list d'objets Trip
+        Liste des passagers qui doivent monter dans le véhicule.
+    boarding_passengers : list d'objets Trip
+        Liste des passagers qui montent dans le véhicule.
+    boarded_passengers : list d'objets Trip
+        Liste des passagers déjà montés dans le véhicule.
+    passengers_to_alight : list d'objets Trip
+        Liste des passagers qui doivent descendre à cet arrêt.
+    alighting_passengers : list d'objets Trip
+        Liste des passagers en train de descendre du véhicule.
+    alighted_passengers : list d'objets Trip
+        Liste des passagers déjà descendus du véhicule.
+    location : Location
+        L'emplacement de l'arrêt, sous forme d'un objet Location (par exemple, coordonnées GPS).
     """
 
     def __init__(self, arrival_time: float, departure_time: float,
                  location: 'Location',
                  cumulative_distance: Optional[float] = None,
                  min_departure_time: Optional[float] = None) -> None:
+        """
+        Initialise l'objet Stop avec des attributs essentiels tels que l'heure d'arrivée, l'heure de départ, 
+        la localisation et les listes pour la gestion des passagers.
+        
+        Paramètres :
+        ----------
+        arrival_time : float
+            Heure à laquelle le véhicule arrive à l'arrêt.
+        departure_time : float
+            Heure à laquelle le véhicule quitte l'arrêt.
+        location : Location
+            L'emplacement de l'arrêt.
+        cumulative_distance : float, optionnel
+            Distance cumulée parcourue par le véhicule lors de son arrivée à l'arrêt.
+        min_departure_time : float, optionnel
+            Heure minimale à laquelle le véhicule peut quitter l'arrêt.
+        """
         super().__init__()
-
+        # Initialisation des attributs de l'arrêt
         self.__arrival_time = arrival_time
         self.__departure_time = departure_time
         self.__min_departure_time = min_departure_time
@@ -50,6 +70,11 @@ class Stop:
         self.__cumulative_distance = cumulative_distance
 
     def __str__(self) -> str:
+        """
+        Retourne une représentation sous forme de chaîne de caractères de l'objet Stop, incluant les IDs de tous 
+        les passagers impliqués (en attente de monter, en train de monter, montés, en attente de descendre, en train 
+        de descendre, et déjà descendus).
+        """
         class_string = str(self.__class__) + ": {"
         for attribute, value in self.__dict__.items():
             if "__passengers_to_board" in attribute:
@@ -76,7 +101,7 @@ class Stop:
         class_string += "}"
 
         return class_string
-
+    # Méthodes d'accès (getters et setters) aux attributs de l'arrêt
     @property
     def arrival_time(self) -> float:
         return self.__arrival_time
@@ -91,6 +116,10 @@ class Stop:
 
     @departure_time.setter
     def departure_time(self, departure_time: float):
+        """
+        Vérifie que l'heure de départ est supérieure ou égale à l'heure minimale de départ autorisée.
+        Si ce n'est pas le cas, une exception ValueError est levée.
+        """
         if self.__min_departure_time is not None \
                 and departure_time < self.__min_departure_time:
             raise ValueError(
@@ -152,27 +181,38 @@ class Stop:
         return self.__cumulative_distance
 
     def initiate_boarding(self, trip: 'request.Trip'):
-        """Passengers who are ready to be picked up in the stop get in the
-        vehicle """
+        """
+        Déplace un passager de la liste des passagers à embarquer vers la liste des passagers en train de monter.
+        """
         self.passengers_to_board.remove(trip)
         self.boarding_passengers.append(trip)
 
     def board(self, trip: 'request.Trip'):
-        """Passenger who is boarding becomes boarded"""
+        """
+        Déplace un passager de la liste des passagers en train de monter vers la liste des passagers déjà montés.
+        """
         self.boarding_passengers.remove(trip)
         self.boarded_passengers.append(trip)
 
     def initiate_alighting(self, trip: 'request.Trip'):
-        """Passengers who reached their stop leave the vehicle"""
+        """
+        Déplace un passager de la liste des passagers à descendre vers la liste des passagers en train de descendre.
+        """
         self.passengers_to_alight.remove(trip)
         self.alighting_passengers.append(trip)
 
     def alight(self, trip: 'request.Trip'):
-        """Passenger who is alighting becomes alighted"""
+        """
+        Déplace un passager de la liste des passagers en train de descendre vers la liste des passagers déjà descendus.
+        """
         self.alighting_passengers.remove(trip)
         self.alighted_passengers.append(trip)
 
     def __deepcopy__(self, memo: dict) -> 'Stop':
+        """
+        Crée une copie profonde de l'objet Stop en copiant tous ses attributs, sauf ceux qui sont liés aux passagers 
+        (qui doivent être réinitialisés).
+        """
         cls = self.__class__
         result = cls.__new__(cls)
         memo[id(self)] = result
@@ -191,26 +231,67 @@ class Stop:
 
 
 class Location:
-    """The ``Location`` class is a base class that mostly serves as a
-    structure for storing basic information about the location of a vehicle
-    or a passenger (i.e., Request). """
+    """
+    La classe Location est une classe de base qui sert principalement à stocker des informations de base sur 
+    l'emplacement d'un véhicule ou d'un passager (par exemple, coordonnées GPS).
+    """
 
     def __init__(self) -> None:
+        """
+        Initialisation d'un objet Location. Cette classe de base ne contient pas d'attributs spécifiques.
+        """
         pass
 
     def __eq__(self, other: 'Location') -> bool:
+        """
+        Compare deux objets Location pour voir s'ils sont égaux. Cette méthode est laissée vide dans la classe de base,
+        mais elle peut être redéfinie dans les classes filles.
+        
+        Paramètre :
+        ----------
+        other : Location
+            Un autre objet Location avec lequel comparer.
+
+        Retourne :
+        --------
+        bool : True si les deux objets sont égaux, sinon False.
+        """
         pass
 
 
 class LabelLocation(Location):
+    """
+    La classe LabelLocation étend la classe Location. Elle représente un emplacement avec un label (nom) et 
+    des coordonnées facultatives (longitude et latitude).
+    """
     def __init__(self, label: str, lon: Optional[float] = None,
                  lat: Optional[float] = None) -> None:
+        """
+        Initialisation d'un objet LabelLocation avec un label et des coordonnées optionnelles.
+
+        Paramètres :
+        ----------
+        label : str
+            Le label (nom) de l'emplacement.
+        lon : float, optionnel
+            La longitude de l'emplacement.
+        lat : float, optionnel
+            La latitude de l'emplacement.
+        """
         super().__init__()
-        self.label = label
-        self.lon = lon
-        self.lat = lat
+        self.label = label # Le label identifiant l'emplacement
+        self.lon = lon # La longitude (facultative)
+        self.lat = lat  # La latitude (facultative)
 
     def __str__(self) -> str:
+        """
+        Retourne une représentation sous forme de chaîne de caractères de l'objet LabelLocation, incluant le label
+        et les coordonnées, si elles sont spécifiées.
+
+        Retourne :
+        --------
+        str : Représentation sous forme de chaîne de caractères de l'emplacement.
+        """
 
         if self.lon is not None or self.lat is not None:
             ret_str = "{}: ({},{})".format(self.label, self.lon, self.lat)
@@ -220,11 +301,35 @@ class LabelLocation(Location):
         return ret_str
 
     def __eq__(self, other: 'LabelLocation') -> bool:
+        """
+        Compare deux objets LabelLocation pour voir s'ils sont égaux, c'est-à-dire si leurs labels sont identiques.
+
+        Paramètre :
+        ----------
+        other : LabelLocation
+            Un autre objet LabelLocation avec lequel comparer.
+
+        Retourne :
+        --------
+        bool : True si les deux objets ont le même label, sinon False.
+        """
         if isinstance(other, LabelLocation):
             return self.label == other.label
         return False
 
     def __deepcopy__(self, memo: dict) -> 'LabelLocation':
+        """
+        Crée une copie profonde de l'objet LabelLocation, y compris ses attributs (label, longitude et latitude).
+
+        Paramètre :
+        ----------
+        memo : dict
+            Un dictionnaire pour mémoriser les objets déjà copiés.
+
+        Retourne :
+        --------
+        LabelLocation : Une copie profonde de l'objet LabelLocation.
+        """
         cls = self.__class__
         result = cls.__new__(cls)
         memo[id(self)] = result
@@ -234,22 +339,71 @@ class LabelLocation(Location):
 
 
 class TimeCoordinatesLocation(Location):
+    """
+    La classe TimeCoordinatesLocation étend la classe Location. Elle représente un emplacement avec des 
+    coordonnées temporelles : un instant de temps, une longitude et une latitude.
+    """
     def __init__(self, time: float, lon: float, lat: float) -> None:
+        """
+        Initialisation d'un objet TimeCoordinatesLocation avec un temps, une longitude et une latitude.
+
+        Paramètres :
+        ----------
+        time : float
+            Le moment dans le temps auquel cet emplacement est enregistré.
+        lon : float
+            La longitude de l'emplacement.
+        lat : float
+            La latitude de l'emplacement.
+        """
         super().__init__()
         self.time = time
         self.lon = lon
         self.lat = lat
 
     def __str__(self) -> str:
+        """
+        Retourne une représentation sous forme de chaîne de caractères de l'objet TimeCoordinatesLocation, incluant
+        le temps, la longitude et la latitude.
+
+        Retourne :
+        --------
+        str : Représentation sous forme de chaîne de caractères de l'emplacement temporel.
+        """
         return "{}: ({},{})".format(self.time, self.lon, self.lat)
 
     def __eq__(self, other) -> bool:
+        """
+        Compare deux objets TimeCoordinatesLocation pour voir s'ils sont égaux, c'est-à-dire si leur temps, longitude
+        et latitude sont identiques.
+
+        Paramètre :
+        ----------
+        other : TimeCoordinatesLocation
+            Un autre objet TimeCoordinatesLocation avec lequel comparer.
+
+        Retourne :
+        --------
+        bool : True si les deux objets ont le même temps, longitude et latitude, sinon False.
+        """
         if isinstance(other, TimeCoordinatesLocation):
             return self.time == other.time and self.lon == other.lon \
                    and self.lat == other.lat
         return False
 
     def __deepcopy__(self, memo: dict) -> 'TimeCoordinatesLocation':
+        """
+        Crée une copie profonde de l'objet TimeCoordinatesLocation, y compris ses attributs (temps, longitude et latitude).
+
+        Paramètre :
+        ----------
+        memo : dict
+            Un dictionnaire pour mémoriser les objets déjà copiés.
+
+        Retourne :
+        --------
+        TimeCoordinatesLocation : Une copie profonde de l'objet TimeCoordinatesLocation.
+        """
         cls = self.__class__
         result = cls.__new__(cls)
         memo[id(self)] = result
