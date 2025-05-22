299036872af0d1f879e6c9ca7498e7dc1951d70e
Ando Rakotonandrasana <ando-sitraka-2.rakotonandrasana@polymtl.ca>
2025-05-22 15:24:40 -0400

    Ando_test

--- python/examples/stl_cap_and_gtfs_generator_month.py
+++ python/examples/stl_cap_and_gtfs_generator_month.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 if __name__ == '__main__':
     # Import STL files (personal) : Source file (replace with your own path)
     # répertoires des donnéée
-    path = r"D:\donnees\Donnees_PASSAGE_ARRET_VLV_2019-11-01_2019-11-30.csv"
+    path = r"D:\Donnees_PASSAGE_ARRET_VLV_2019-11-01_2019-11-30\Donnees_PASSAGE_ARRET_VLV_2019-11-01_2019-11-30_0.csv"
     passage_arret_file_path_list = [path]
     # Destination folder
     gtfs_folder = os.path.join("data","fixed_line","gtfs","gtfs")
@@ -22,31 +22,31 @@ if __name__ == '__main__':
     #Generate GTFS files (do once)
     gtfs_generator = GTFSGenerator()
     logger.info("build_calendar_dates")
-    # gtfs_generator.build_calendar_dates(passage_arret_file_path_list=passage_arret_file_path_list, gtfs_folder=gtfs_folder)
-    # logger.info("build_trips")
-    # gtfs_generator.build_trips(passage_arret_file_path_list=passage_arret_file_path_list, gtfs_folder=gtfs_folder)
-    # logger.info("build_stops")
-    # gtfs_generator.build_stops(passage_arret_file_path_list=passage_arret_file_path_list, gtfs_folder=gtfs_folder)
-    # logger.info("build_stop_times")
-    # gtfs_generator.build_stop_times(passage_arret_file_path_list=passage_arret_file_path_list, gtfs_folder=gtfs_folder, shape_dist_traveled=False)
-    # logger.info("build_stop_times_upgrade")
-    # gtfs_generator.build_stop_times_upgrade(passage_arret_file_path_list=passage_arret_file_path_list, gtfs_folder=gtfs_folder, shape_dist_traveled=True)
-    # logger.info("Done importing GTFS files")
+    gtfs_generator.build_calendar_dates(passage_arret_file_path_list=passage_arret_file_path_list, gtfs_folder=gtfs_folder)
+    logger.info("build_trips")
+    gtfs_generator.build_trips(passage_arret_file_path_list=passage_arret_file_path_list, gtfs_folder=gtfs_folder)
+    logger.info("build_stops")
+    gtfs_generator.build_stops(passage_arret_file_path_list=passage_arret_file_path_list, gtfs_folder=gtfs_folder)
+    logger.info("build_stop_times")
+    gtfs_generator.build_stop_times(passage_arret_file_path_list=passage_arret_file_path_list, gtfs_folder=gtfs_folder, shape_dist_traveled=False)
+    logger.info("build_stop_times_upgrade")
+    gtfs_generator.build_stop_times_upgrade(passage_arret_file_path_list=passage_arret_file_path_list, gtfs_folder=gtfs_folder, shape_dist_traveled=True)
+    logger.info("Done importing GTFS files")
 
-    ## Split large .csv file into daily files (do once and then comment out)
-    # logger.info("Split large .csv file into daily files")
-    # ## Source file (replace with your own path)
-    # passage_arret_df = pd.read_csv(r"D:\donnees\Donnees_CAP_GFI_SPOT_2019-11-01_2019-11-30\Donnees_CAP_GFI_SPOT_2019-11-01_2019-11-30.csv", delimiter = ',')
-    # new_cap_folder = os.path.join("D:","donnees","New donnees")
-    # dates_list = passage_arret_df['DATE28'].unique()
-    # for date in dates_list:
-    #     trips_day_df = passage_arret_df[
-    #         passage_arret_df['DATE28'] == date].drop('DATE28', axis=1)
-    #     cap_filename = date.split(" ")[0].replace("-", "") + ".csv"
-    #     trips_day_df.to_csv(os.path.join(new_cap_folder, cap_filename), index = None, sep = ';')
+    # Split large .csv file into daily files (do once and then comment out)
+    logger.info("Split large .csv file into daily files")
+    ## Source file (replace with your own path)
+    passage_arret_df = pd.read_csv(r"D:\Donnees_CAP_GFI_SPOT_2019-11-01_2019-11-30\Donnees_CAP_GFI_SPOT_2019-11-01_2019-11-30.csv", delimiter = ',')
+    new_cap_folder = os.path.join("D:","donnees","New donnees")
+    dates_list = passage_arret_df['DATE28'].unique()
+    for date in dates_list:
+        trips_day_df = passage_arret_df[
+            passage_arret_df['DATE28'] == date].drop('DATE28', axis=1)
+        cap_filename = date.split(" ")[0].replace("-", "") + ".csv"
+        trips_day_df.to_csv(os.path.join(new_cap_folder, cap_filename), index = None, sep = ';')
     
-    # Extract all lines from GTFS files
-    # all_lines_SN, all_lines_EO = gtfs_generator.get_all_lines()
+    #Extract all lines from GTFS files
+    all_lines_SN, all_lines_EO = gtfs_generator.get_all_lines()
 
     ##  Extract available connections from CAP Data (do once and then comment out)
     ## If you want to change release_time_delta, ready_time_delta, due_time_delta used in the optimization, you have to regenerate this part.
